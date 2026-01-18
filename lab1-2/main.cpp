965f7839de0caef430ca6407fc95d4ffa9a9a097
sophie-lanuit <sofigaran-ipt28@lll.kpi.ua>
2026-01-18 04:01:30 +0200

    update lab2

--- lab1-2/main.cpp
+++ lab1-2/main.cpp
@@ -8,6 +8,10 @@ void timer() {
     Long_uint a = Long_uint::from_hex("0123456789ABCDEF");
     Long_uint b = Long_uint::from_hex("0123456789ABCDEF");
     const int N = 10000;
+    Long_uint n = Long_uint::from_hex("100");
+    Long_uint m = Long_uint::barrett_mu(n);
+    Long_uint q, r;
+    Long_uint exp(17);
 
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; ++i) Long_uint::add(a, b);
@@ -17,35 +21,83 @@ void timer() {
     for (int i = 0; i < N; ++i) Long_uint::multiply(a, b);
     auto t4 = std::chrono::high_resolution_clock::now();
 
-    Long_uint q, r;
     auto t5 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; ++i) Long_uint::divmod(a, b, q, r);
     auto t6 = std::chrono::high_resolution_clock::now();
 
-    Long_uint exp(17);
     auto t7 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; ++i) Long_uint::power(a, exp);
     auto t8 = std::chrono::high_resolution_clock::now();
+
+    auto t9 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i) Long_uint::gcd(a, b);
+    auto t10 = std::chrono::high_resolution_clock::now();
+
+    auto t11 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i) Long_uint::lcm(a, b);
+    auto t12 = std::chrono::high_resolution_clock::now();
+
+    auto t13 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) Long_uint::add_mod(a, b, n);
+    auto t14 = std::chrono::high_resolution_clock::now();
+
+    auto t15 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) Long_uint::sub_mod(a, b, n);
+    auto t16 = std::chrono::high_resolution_clock::now();
+
+    auto t17 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) Long_uint::mul_mod(a, b, n, m);
+    auto t18 = std::chrono::high_resolution_clock::now();
+
+    auto t19 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) Long_uint::sq_mod(a, n, m);
+    auto t20 = std::chrono::high_resolution_clock::now();
+
+    Long_uint large = Long_uint::multiply(a, b);
+    auto t21 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) Long_uint::barrett_reduce(large, n, m);
+    auto t22 = std::chrono::high_resolution_clock::now();
+
+    auto t23 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i) Long_uint::pow_mod(a, exp, n);
+    auto t24 = std::chrono::high_resolution_clock::now();
 
     auto add_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / N;
     auto mul_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t4 - t3).count() / N;
     auto div_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t6 - t5).count() / N;
     auto pow_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t8 - t7).count() / 100;
+    auto gcd_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t10 - t9).count() / 100;
+    auto lcm_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count() / 100;
+    auto add_mod_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t14 - t13).count() / N;
+    auto sub_mod_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t16 - t15).count() / N;
+    auto mul_mod_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t18 - t17).count() / N;
+    auto sq_mod_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t20 - t19).count() / N;
+    auto barrett_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t22 - t21).count() / N;
+    auto pow_mod_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t24 - t23).count() / 100;
 
     std::cout << "\nTime:\n";
     std::cout << "Add: " << add_ns << " ns\n";
     std::cout << "Mul: " << mul_ns << " ns\n";
     std::cout << "Div: " << div_ns << " ns\n";
     std::cout << "Pow: " << pow_ns << " ns\n";
+    std::cout << "gcd: " << gcd_ns << " ns\n";
+    std::cout << "lcm: " << lcm_ns << " ns\n";
+    std::cout << "Add_mod: " << add_mod_ns << " ns\n";
+    std::cout << "Sub_mod: " << sub_mod_ns << " ns\n";
+    std::cout << "Mul_mod: " << mul_mod_ns << " ns\n";
+    std::cout << "Sq_mod: " << sq_mod_ns << " ns\n";
+    std::cout << "Barrett reduce: " << barrett_ns << " ns\n";
+    std::cout << "Pow_mod: " << pow_mod_ns << " ns\n";
 }
 
 void timer_cycles() {
     Long_uint a = Long_uint::from_hex("0123456789ABCDEF");
     Long_uint b = Long_uint::from_hex("0123456789ABCDEF");
-
     const int N = 10000;
     Long_uint q, r;
     Long_uint exp(17);
+    Long_uint n = Long_uint::from_hex("100");
+    Long_uint m = Long_uint::barrett_mu(n);
 
     unsigned long long t1, t2;
 
@@ -69,11 +121,60 @@ void timer_cycles() {
     t2 = __rdtsc();
     auto pow_cycles = (t2 - t1) / 100;
 
+    t1 = __rdtsc();
+    for (int i = 0; i < 100; ++i) Long_uint::gcd(a, b);
+    t2 = __rdtsc();
+    auto gcd_cycles = (t2 - t1) / 100;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < 100; ++i) Long_uint::lcm(a, b);
+    t2 = __rdtsc();
+    auto lcm_cycles = (t2 - t1) / 100;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < N; ++i) Long_uint::add_mod(a, b, n);
+    t2 = __rdtsc();
+    auto add_mod_cycles = (t2 - t1) / N;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < N; ++i) Long_uint::sub_mod(a, b, n);
+    t2 = __rdtsc();
+    auto sub_mod_cycles = (t2 - t1) / N;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < N; ++i) Long_uint::mul_mod(a, b, n, m);
+    t2 = __rdtsc();
+    auto mul_mod_cycles = (t2 - t1) / N;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < N; ++i) Long_uint::sq_mod(a, n, m);
+    t2 = __rdtsc();
+    auto sq_mod_cycles = (t2 - t1) / N;
+
+    Long_uint large = Long_uint::multiply(a, b);
+    t1 = __rdtsc();
+    for (int i = 0; i < N; ++i) Long_uint::barrett_reduce(large, n, m);
+    t2 = __rdtsc();
+    auto barrett_cycles = (t2 - t1) / N;
+
+    t1 = __rdtsc();
+    for (int i = 0; i < 100; ++i) Long_uint::pow_mod(a, exp, n);
+    t2 = __rdtsc();
+    auto pow_mod_cycles = (t2 - t1) / 100;
+
     std::cout << "\nProcessor ticks:\n";
     std::cout << "Add: " << add_cycles << "\n";
     std::cout << "Mul: " << mul_cycles << "\n";
     std::cout << "Div: " << div_cycles << "\n";
     std::cout << "Pow: " << pow_cycles << "\n";
+    std::cout << "gcd: " << gcd_cycles << "\n";
+    std::cout << "lcm: " << lcm_cycles << "\n";
+    std::cout << "Add_mod: " << add_mod_cycles << "\n";
+    std::cout << "Sub_mod: " << sub_mod_cycles << "\n";
+    std::cout << "Mul_mod: " << mul_mod_cycles << "\n";
+    std::cout << "Sq_mod: " << sq_mod_cycles << "\n";
+    std::cout << "Barrett: " << barrett_cycles << "\n";
+    std::cout << "Pow_mod: " << pow_mod_cycles << "\n";
 }
 
 int main() {
